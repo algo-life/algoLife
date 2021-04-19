c61ea53ac0c0fccc312cbc3241f95697b3a7eb0f
Weilan <weilanc@gmail.com>
2021-04-19 17:14:15 -0400

    setup react-router

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+
 export interface HelloWorldProps {
   userName: string;
   lang: string;
 }
+
+
+{/* <h1>
+Hi {props.userName} from React! Welcome to {props.lang}!
+</h1> */}
+
 export const App = (props: HelloWorldProps) => (
-  <h1>
-    Hi {props.userName} from React! Welcome to {props.lang}!
-  </h1>
+  <Router>
+ <Switch>
+   <Route path='/login'>
+     
+   </Route>
+   <Route path= 'signup' />
+   <Route path='/main'/>
+ </Switch>
+
+
+  </Router>
+
 );
---
+++ client/store.ts
@@ -0,0 +1,7 @@
+import { applyMiddleware, createStore, compose} from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducers from './reducers/index'
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+
+export default store
