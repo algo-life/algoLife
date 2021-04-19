ec70d9862a7e26d606e195465edd733298d9298f
Weilan <weilanc@gmail.com>
2021-04-19 19:01:44 -0400

    finish boilerplate

---
+++ client/components/MainContainer.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function MainContainer() {
+  return (
+    <div>
+      <h1>MainContainer</h1>
+    </div>
+  );
+}
---
+++ client/components/SignUp.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function SignUp() {
+  return (
+    <div>
+      <h1>SignUp</h1>
+    </div>
+  );
+}
