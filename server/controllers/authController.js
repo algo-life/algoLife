42c59f292c53fa65fb6bd6a592efc876b0542c17
Rachel Farley <rachyl.farley@gmail.com>
2021-04-20 16:02:44 -0600

    added authentication middleware

---
+++ server/controllers/authController.js
@@ -0,0 +1,94 @@
+const db = require('../models/model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = process.env.SECRET;
+
+const authController = {};
+
+authController.create = (req, res, next) => {
+  console.log('inside authController.create');
+
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return next({
+      log: 'Error in authController.create',
+      status: 400,
+      message: 'Please provide a valid username and password.',
+    });
+  }
+
+  const saltRounds = 10;
+
+  bcrypt.hash(password, saltRounds).then((hash) => {
+    const query = `
+      INSERT INTO users (username, password)
+      VALUES ($1, $2)
+      RETURNING _id, username;
+      `;
+    db.query(query, [username, hash]).then((response) => {
+      res.locals.newUser = response.rows[0];
+      console.log('res.locals:', res.locals.newUser);
+      return next();
+    });
+  });
+};
+
+authController.login = (req, res, next) => {
+  console.log('inside authController.login');
+
+  const { username, password } = req.body;
+  const query = `
+    SELECT username, password
+    FROM users
+    WHERE username = $1`;
+
+  db.query(query, [username])
+    .then((response) => {
+      bcrypt.compare(password, response.rows[0].password, (err, result) => {
+        if (err) {
+          return next({
+            log: 'Error in authController.login',
+            status: 401,
+            message: 'ERROR: Please provide a valid password.',
+          });
+        }
+
+        if (!result) return res.status(401).json('Incorrect password.');
+        if (result) return next();
+        return next();
+      });
+    })
+    .catch((err) => {
+      console.log('.catch in authController.login', err);
+      return next(err);
+    });
+};
+
+authController.createJWT = (req, res, next) => {
+  console.log('inside authController.createJWT');
+
+  const { username } = req.body;
+
+  jwt.sign({ username }, secret, { expiresIn: '3600s' }, (err, token) => {
+    if (err) return res.status(400).json('Error creating JWT');
+    res.cookie('jwt', token, { httpOnly: true });
+    return next();
+  });
+};
+
+// authController.verifyJWT = (req, res, next) => {
+//   console.log('inside authController.verifyJWT');
+//   console.log('req.cookie: ', req.cookie);
+//   const token = req.cookie.jwt;
+//   console.log('verifyJWT token: ', token);
+//   return next();
+// };
+
+authController.logout = (req, res, next) => {
+  console.log('inside authController.logout');
+  res.cookie('jwt', null);
+  return next();
+};
+
+module.exports = authController;
