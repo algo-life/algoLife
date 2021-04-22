a67e30ebb8bfc395c2100d3bf0a0adb7ea89387c
ryanmcd <rp.mcdan@gmail.com>
2021-04-22 11:47:45 -0400

    adding algo and test

--- client/browserTest/testString.ts
+++ client/browserTest/testString.ts
@@ -1,5 +1,22 @@
-export default `describe('sum', function () {
-    it('should return sum of arguments', function () {
-      chai.expect(sum(1, 2)).to.equal(3);
+describe("addTwo", function () {
+  it("should return the input number plus 2", function () {
+    chai.expect(addTwo(2)).to.equal(4);
+    chai.expect(addTwo(0)).to.equal(2);
+    chai.expect(addTwo(-3)).to.equal(-1);
+  });
+});
+
+describe("isNegativeOrOdd", function () {
+  it("should return true if number is odd", function () {
+    chai.expect(addTwo(2)).to.equal(false);
+    chai.expect(addTwo(-10)).to.equal(true);
+    chai.expect(addTwo(9)).to.equal(true);
+  });
+});
+
+  describe('isNegativeOrOdd', () => {
+    it('should return true if number is odd', () => {
+      expect(isNegativeOrOdd(1)).to.eql(true);
+      expect(isNegativeOrOdd(3)).to.eql(true);
+      expect(isNegativeOrOdd(5)).to.eql(true);
     });
-  });`;
